16975a3bc62a80c3574e6bba4495c44d6407ff46
Murilo Besson <murilo.besson@outlook.com>
2024-09-06 14:54:47 -0300

    Aula 67

---
+++ src/main/java/org/example/StockProgram.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import entities.Product;
+import java.util.Locale;
+import java.util.Scanner;
+public class StockProgram {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Product product = new Product();
+        System.out.println("Enter product name: ");
+        System.out.print("Name: ");
+        product.name = sc.nextLine();
+        System.out.print("Price: ");
+        product.price = sc.nextDouble();
+        System.out.println("Quantity in stock: ");
+        product.quantity = sc.nextInt();
+
+        System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+
+        sc.close();
+    }
+}
